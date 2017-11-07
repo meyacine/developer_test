46fed7fa94e1b8111c5c6887120c0221a581dd63
ymeddah <my.meddah@gmail.com>
2017-11-07 21:33:01 +0100

    bootstrap of students repo > for test purpose only, don't do that on a real project :)

---
+++ _server/repositories/students.js
@@ -0,0 +1,30 @@
+'use strict';
+
+exports.students = [
+    {
+        id : '1',
+        lastname : 'Arya',
+        firstname : 'Stark'
+    },
+    {
+        id : '2',
+        lastname : 'John',
+        firstname : 'SNOW'
+    },
+    {
+        id : '3',
+        lastname : 'Jamie',
+        firstname : 'Lannister'
+    },
+    {
+        id : '4',
+        lastname : 'Daenerys',
+        firstname : 'Targaryen'
+    },
+    {
+        id : '5',
+        lastname : 'Cercei',
+        firstname : 'Lannister'
+    }
+]
+
