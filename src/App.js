ce8f69504dd997152aca4233e69364750a9bd00b
ymeddah <my.meddah@gmail.com>
2017-11-07 22:11:58 +0100

    starts by displaying a list on a table

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            students: [
+                {
+                    id: '1',
+                    lastname: 'Arya',
+                    firstname: 'Stark'
+                },
+                {
+                    id: '2',
+                    lastname: 'John',
+                    firstname: 'SNOW'
+                },
+                {
+                    id: '3',
+                    lastname: 'Jamie',
+                    firstname: 'Lannister'
+                },
+                {
+                    id: '4',
+                    lastname: 'Daenerys',
+                    firstname: 'Targaryen'
+                },
+                {
+                    id: '5',
+                    lastname: 'Cercei',
+                    firstname: 'Lannister'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Students class</h1>
+                </header>
+                <div className="col-md-6">
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>FIRSTNAME</th>
+                            <th>LASTNAME</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.state.students.map(student =>
+                            <tr key={student.id}>
+                                <td>{student.id}</td>
+                                <td>{student.lastname}</td>
+                                <td>{student.firstname}</td>
+                            </tr>
+                        )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
